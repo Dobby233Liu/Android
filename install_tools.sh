20db99dd94541bb1df6a3e0f9d46dae0d7354fd6
Liu-Wen-Yuan <15816141883@163.com>
2020-05-26 06:22:11 +0800

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,12 +1,13 @@
+#!/usr/bin/env bash
 mkdir "$ANDROID_HOME/licenses" || true;
 echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license";
 echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license";
 echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license";
 echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "$ANDROID_HOME/licenses/android-sdk-license";
-sdkmanager 'ndk;21.0.6113669'
-sdkmanager tools;
-yes | sdkmanager --licenses;
+sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager 'ndk;21.0.6113669'
+sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager tools;
 echo "Fetching ndk-bundle. Suppressing output to avoid travis 4MG size limit";
-sdkmanager "ndk-bundle" >/dev/null;
+sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager "ndk-bundle" >/dev/null;
 echo "Fetching ndk-bundle complete";
-sdkmanager "system-images;android-22;default;armeabi-v7a";
+sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager "system-images;android-22;default;armeabi-v7a";
+yes | sdkmanager --licenses;
